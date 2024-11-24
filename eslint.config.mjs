819b289a896eaa0f4bf680745f5ca517ed61b39f
Alex McTammany <apmctammany@gmail.com>
2024-11-23 23:52:25 -0500

    eslinting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,13 +18,18 @@ export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     ignores: ["./.next/**/*", "**/*.config.mjs"],
+    //plugins: { react: pluginReact },
   },
   {
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.serviceworker },
+      ...pluginReact.configs.flat.recommended.languageOptions,
+    },
   },
   //pluginJs.configs.recommended,
   //...tseslint.configs.recommended,
-  //pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"], // Add this if you are using React 17+
   ...compat.extends("next/core-web-vitals"),
   //...compat.extends("next/typescript"),
   //...compat.extends("plugin:@tanstack/eslint-plugin-query/recommended"),
@@ -34,6 +39,8 @@ export default [
       "no-unused-vars": "warn",
       "no-undef": "warn",
       "import/no-anonymous-default-export": "off",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
     },
   },
 ];
