4c3ab90d3db4ff7409a1a8f81571633748f9fb6b
Alex McTammany <apmctammany@gmail.com>
2024-11-26 00:29:48 -0500

    fuck eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,59 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginNext from "eslint-config-next";
+//import pluginNext from "eslint-config-next";
 import tseslint from "typescript-eslint";
+//import configNext from "eslint-plugin-next";
 import pluginReact from "eslint-plugin-react";
 import url from "node:url";
 import eslint from "@eslint/js";
+//import configNext from "eslint-config-next";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const compat = new FlatCompat({ baseDirectory: __dirname });
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
-export default [tseslint.configs.recommended, ...compat.extends("next")];
+//export default [tseslint.configs.recommended, ...compat.extends("next")];
 /** @type {import('eslint').Linter.Config[]} */
 const f = [
   //{ extends: [pluginNext] },
 
-  pluginNext.configs.recommended,
+  //pluginNext.configs.recommended,
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], ignores: [".next/*"] },
   { languageOptions: { globals: globals.browser } },
   //pluginNext.configs.flat.recommended,
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  //...compat.extends("next/typescript"),
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+      "import/no-anonymous-default-export": "off",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
+//export default config;
+export default tseslint.config(
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ignores: ["./.next/**/*", "**/*.config.mjs"],
+    extends: [tseslint.configs.recommended ],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.serviceworker },
+      ...pluginReact.configs.flat.recommended.languageOptions,
+    },
+
+  },
+  pluginReact.configs.flat.recommended,
+  //configNext.configs.flat.recommended, // .flat.configs.recommended,
+  tseslint.configs.recommended,
+
+  ...compat.extends("next/typescript"),
   {
     rules: {
       "no-unused-vars": "off",
@@ -33,6 +64,5 @@ const f = [
       "react/jsx-uses-react": "off",
       "react/react-in-jsx-scope": "off",
     },
-  },
-];
-
+  }
+);
