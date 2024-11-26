2bbe0382130cbf43ee317c296ec9bd47b13b8fba
Alex McTammany <apmctammany@gmail.com>
2024-11-26 00:24:28 -0500

    fuck

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,25 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import pluginNext from "eslint-config-next";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import url from "node:url";
+import eslint from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [tseslint.configs.recommended, ...compat.extends("next")];
 /** @type {import('eslint').Linter.Config[]} */
-export default [
+const f = [
+  //{ extends: [pluginNext] },
+
+  pluginNext.configs.recommended,
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], ignores: [".next/*"] },
   { languageOptions: { globals: globals.browser } },
+  //pluginNext.configs.flat.recommended,
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
